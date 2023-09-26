53e30f6decdcbe7fdbbc6e037dc9f1f2b4bd2383
User <stivens.kokainis@edu.rtu.lv>
2023-09-26 10:46:37 +0300

    Class_5a

---
+++ Class_05/second.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+// vienas rindas komentārs
+/* komentāra bloka sākums
+...
+...
+komentāra bloka beigas */
+int main()
+ {
+
+ printf("Hello World\n"); // \n - dabības simbols - kursora parcelšana
+                         // jaunaja rindā
+                        // analogija ar echo -e "Hello World!\n"
+ printf("\a");         // \a - darbības sibols - skaņa
+ return 0;
+ }
